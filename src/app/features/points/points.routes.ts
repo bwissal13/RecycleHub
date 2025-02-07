932aff0b70118a6340ddbea4591259f28cbd455f
bwissal13 <baazizwissal13@gmail.com>
2025-02-07 10:18:06 +0100

    REC-26: Implement points system and rewards functionality

---
+++ src/app/features/points/points.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PointsDashboardComponent } from './points-dashboard/points-dashboard.component';
+
+export const POINTS_ROUTES: Routes = [
+  {
+    path: '',
+    component: PointsDashboardComponent
+  }
+]; 
