11b8c018fe00fc0e1e8a1d7e8674ec644020ff24
bwissal13 <baazizwissal13@gmail.com>
2025-02-15 17:20:14 +0100

    add

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NavComponent } from '../../shared/components/nav/nav.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NavComponent],
   templateUrl: './landing.component.html',
   styleUrls: ['../../styles/components/landing.css']
 })
